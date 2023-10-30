5353b47d4b728439fb9ac8a63114c22d4958de34
Nehel Khanna <nehel.khanna20@st.niituniversity.in>
2023-10-30 12:21:15 +0530

    navbar jsx component

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function navbar(type){
+    return(
+        <div className="nav">
+            <div className="nav-h1">
+                {(type==1)? stag-me : unstag-me}
+            </div>
+        </div>
+    )
+}
